3684b574d665ea4f604ea63a2b4684c62d723154
Ash <chipinterm@gmail.com>
2022-02-17 10:18:48 -0800

    Some Calculator File Uploads

---
+++ main.js
@@ -0,0 +1,57 @@
+//old 1st try
+
+//const allClearButton =
+//const deleteButton =
+//const equalsButton=
+//const operationsButton =
+//const numberButton=
+
+//multiplication
+//
+//function multiply(x,y){
+//    return x*y;
+//}
+//
+////division
+//
+//function divide(x,y){
+//    return x/y;
+//}
+//
+////addition
+//
+//function add(x,y){
+//    return x+y;
+//}
+//
+////subtraction
+//
+//function subtract(x,y){
+//    return x-y;
+//}
+//
+//Made By: https://github.com/Tsarina-Ash
+
+//multiplication
+
+function multiply(x,y){
+    return x*y;
+}
+
+//division
+
+function divide(x,y){
+    return x/y;
+}
+
+//addition
+
+function add(x,y){
+    return x+y;
+}
+
+//subtraction
+
+function subtract(x,y){
+    return x-y;
+}
