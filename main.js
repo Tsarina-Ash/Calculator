86a70ac02de620a53be9ae3f0190b17abed20444
Ash <chipinterm@gmail.com>
2022-02-18 09:37:19 -0800

    Massive Upload
    Uploaded new background
    
    One functioning calculator
    
    New header

--- main.js
+++ main.js
@@ -1,57 +1,70 @@
-//old 1st try
+  ///////////////////////
+ ///SIMPLE CALCULATOR///
+///////////////////////
 
-//const allClearButton =
-//const deleteButton =
-//const equalsButton=
-//const operationsButton =
-//const numberButton=
+        //defining what "sub-area" does what
 
-//multiplication
-//
-//function multiply(x,y){
-//    return x*y;
-//}
-//
-////division
-//
-//function divide(x,y){
-//    return x/y;
-//}
-//
-////addition
-//
-//function add(x,y){
-//    return x+y;
-//}
-//
-////subtraction
-//
-//function subtract(x,y){
-//    return x-y;
-//}
-//
-//Made By: https://github.com/Tsarina-Ash
+        let form = document.getElementById('demoForm'), 
+		formData = form.elements; 
+console.log(formData);
 
-//multiplication
+        //defining X/Y/OP
 
-function multiply(x,y){
-    return x*y;
+function showData(event){
+	let x = parseInt(formData['x'].value),
+			y = parseInt(formData['y'].value),
+			op = formData['operation'].value, 
+			out;
+	
+        //defining the equations
+
+	let output = document.getElementById('output');
+
+        function add(x,y){
+            return x+y;
+        }
+
+        function subtract(x,y){
+            return x-y;
+        }
+
+        function multiply(x,y){
+            return x*y;
+        }
+
+        function divide(x,y){
+            return x/y;
+        }
+
+        //allows proper switching between submatters
+
+	switch(op){
+		case 'add':
+			out = add(x,y);
+		break;
+		case 'subtract':
+			out = subtract(x,y);
+		break;
+		case 'add':
+			out = multiply(x,y);
+		break;
+		case 'subtract':
+			out = divide(x,y);
+		break;        
+	}
+
+        //"dispensing" the answer
+
+	output.innerHTML = `${x} ${op} ${y} = ${out} `;
+
+        //prevents page from auto-refreshing
+
+	event.preventDefault();
+
 }
 
-//division
+form.addEventListener('submit',showData) // this "listens" for the submit button to be clicked and triggers our "showData" function when it is.
 
-function divide(x,y){
-    return x/y;
-}
-
-//addition
-
-function add(x,y){
-    return x+y;
-}
-
-//subtraction
-
-function subtract(x,y){
-    return x-y;
-}
+  ////////////////////////
+ ///COMPLEX CALCULATOR///
+////////////////////////
